575139a0f9be5e083a75796e1a683c2ca187a023
Lorena Rabelo <ola.lorenarabelo@gmail.com>
2021-04-04 18:31:42 -0300

    Atualiza arquivo da aula - refatoração do código

--- projeto-guiado/src/script.js
+++ projeto-guiado/src/script.js
@@ -1,8 +1,6 @@
-//Utilizar const e/ou let
-
-var entrada = document.getElementById("input");
-var saida = document.getElementById("output");
-var chaveDeslocamento = document.getElementById("shift-input");
+let entrada = document.getElementById("input");
+let saida = document.getElementById("output");
+let chaveDeslocamento = document.getElementById("shift-input");
 
 function limpar() {
     entrada.value = "";
@@ -11,21 +9,17 @@ function limpar() {
     return;
 }
 
-// CHAVE DE DESLOCAMENTO - INDEX INICIAL;
-
-// 
 //     A  B  C  D  E  F  G  H  I  J  K  L  M
 //     0  1  2  3  4  5  6  7  8  9  10 11 12
 
 //     N  O  P  Q  R  S  T  U  V  W  X  Y  Z
 //    13 14 15 16 17 18 19 20 21 22 23  24  25
 
+
 // const letrasMaiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-
-
 const letrasMaiusculas = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// exemplo de código que faz a mesma coisa
+// exemplo de código que pega o valor de um texto digitado pelo usuário
 // var texto = prompt("Digite um texto: ");
 // var texto = entrada.value;
 
@@ -38,27 +32,21 @@ function cifrar() {
     //     console.log(palavras[i], "texto genérico palavra");
     // }
 
-    "";
     let cifrado = "";
+    let deslocamento = Number(chaveDeslocamento.value);
 
-    for (itemPalavras of palavras) {
+    if (deslocamento && palavras.length > 0) {
+        for (itemPalavras of palavras) {
 
-        let chave = Number(chaveDeslocamento.value);
-        if (chave !== 0) {
-
-
-
+            //verifica se chaveDeslocamento existe
+            // ver artigo https://blog.rocketseat.com.br/entendendo-falsy-e-truthy-no-javascript/
             for (letra of itemPalavras) {
-                // PRECISAMOS PEGAR O INDEX DA LETRA NO ARRAY COM O ALFABETO
+                // precisamos pegar o index da letra no array do alfabeto
                 const indexInicial = letrasMaiusculas.indexOf(letra);
-
-                let indexFinal;
-                // criar um if else , onde se não for encontrado no array de letras maiusculas
-                // retorne um alert avisando, do contrario dê um console.log
                 if (indexInicial !== -1) {
                     // vamos somar o index inicial com chave de deslocamento
                     // % retorna o resto da divisão
-                    indexFinal = (indexInicial + Number(chaveDeslocamento.value)) % 26;
+                    let indexFinal = (indexInicial + deslocamento) % 26;
                     // cifrar = cifrar +  letrasMaiusculas[indexFinal]
                     cifrado += letrasMaiusculas[indexFinal];
                 } else {
@@ -68,57 +56,52 @@ function cifrar() {
                     return;
                 }
             }
-        } else {
-            alert("Digite uma chave");
+            cifrado += " ";
         }
-        cifrado += " ";
+        saida.value = cifrado;
+        return;
+    } else {
+        alert("Digite uma chave e um texto para decifrar");
     }
-    saida.value = cifrado;
-    return;
 }
 
-
-// - criar função decifrar
-//     - criar variavel que receba o texto, e a transforme em Array;
-// -em um laço cada dê um console.log item desse array de palavras;
 
 function decifrar() {
     const palavrasDecifrar = entrada.value.split(" ");
     let decifrado = "";
+    let deslocamento = Number(chaveDeslocamento.value);
 
-    for (palavra of palavrasDecifrar) {
+    if (deslocamento && palavrasDecifrar.length > 0) {
+        for (palavra of palavrasDecifrar) {
+            // laço que itera a palavra - letra a letra
+            for (letra of palavra) {
 
-        // laço que itera a palavra - letra a letra
-        for (letra of palavra) {
-            console.log("letra : ", letra);
-            const indexInicial = letrasMaiusculas.indexOf(letra);
-            let indexFinal;
-            console.log(indexInicial, "indexInicial");
+                const indexInicial = letrasMaiusculas.indexOf(letra);
+                let indexFinal;
 
-            // A CONDIÇÃO GARANTE QUE O INDEX FINAL É POSITIVO;
-            if (indexInicial !== -1 && indexInicial >= Number(chaveDeslocamento.value)) {
-                indexFinal = (indexInicial - Number(chaveDeslocamento.value));
-                console.log(indexFinal, "indexFinal");
-                // DECIFRADO = DECIFRADO + letrasMaiusculas[indexFinal]
-                decifrado += letrasMaiusculas[indexFinal];
-                // A CONDIÇÃO GARANTE QUE A LETRA EXISTE NO ARRAY E A CHAVE DE DESLOCAMENTO É MAIOR QUE QUE O INDEX INICIAL;
-                // } else if (indexInicial !== -1) {
-            } else if (indexInicial !== -1 && Number(chaveDeslocamento.value) > indexInicial) {
-                indexFinal = letrasMaiusculas.length - (Number(chaveDeslocamento.value) - indexInicial);
-                console.log("indexInicial ", indexInicial);
-                console.log("indexFinal ", indexFinal);
-                decifrado += letrasMaiusculas[indexFinal];
-            } else {
-                // TUDO O QUE NAO ENCONTRAMOS NO ARRAY DO ALFABETO
-                console.log(indexInicial, "indexInicial");
-                alert("Poxa, não encontrei o que digitou");
-                limpar();
-                return;
+                // a condição garante que o index final é positivo
+                if (indexInicial !== -1 && indexInicial >= deslocamento) {
+                    indexFinal = (indexInicial - deslocamento);
+
+                    // decifrado = decifrado + letrasMaiusculas[indexFinal]
+                    decifrado += letrasMaiusculas[indexFinal];
+                    // a condição garante que a letra existe no array e a chave de deslocamento é maior que o index inicial;
+                    // a linha abaixo deixa mais limpo no lugar da linha 97 
+                    // } else if (indexInicial !== -1) {
+                } else if (indexInicial !== -1 && deslocamento > indexInicial) {
+                    indexFinal = letrasMaiusculas.length - (deslocamento - indexInicial);
+                    decifrado += letrasMaiusculas[indexFinal];
+                } else {
+                    // quando não encontra a letra no alfabeto, entra no else
+                    alert("Poxa, não encontrei o que digitou");
+                    limpar();
+                    return;
+                }
             }
+            decifrado += " ";
         }
-        decifrado += " ";
+        saida.value = decifrado;
+    } else {
+        alert("Digite uma chave e um texto para decifrar");
     }
-    console.log(decifrado, "decifrado");
-    saida.value = decifrado;
-    return;
 }
