c03fce40b4db52cfe857c50033011a870ddd377c
Lorena Rabelo <ola.lorenarabelo@gmail.com>
2021-04-05 20:14:17 -0300

    Adiciona segunda versão do exercício

--- projeto-guiado/src/script.js
+++ projeto-guiado/src/script.js
@@ -1,78 +1,63 @@
-let entrada = document.getElementById("input");
-let saida = document.getElementById("output");
-let chaveDeslocamento = document.getElementById("shift-input");
+const entrada = prompt("Digite um texto a ser cifrado");
 
-function limpar() {
-    entrada.value = "";
-    chaveDeslocamento.value = "";
-    saida.value = "";
-    return;
-}
+const chaveDeslocamento = Number(prompt("Digite um número para a chave de deslocamento"));
 
-//     A  B  C  D  E  F  G  H  I  J  K  L  M
-//     0  1  2  3  4  5  6  7  8  9  10 11 12
-
-//     N  O  P  Q  R  S  T  U  V  W  X  Y  Z
-//    13 14 15 16 17 18 19 20 21 22 23  24  25
-
-
-// const letrasMaiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+// outra opção de gerar o array >> const letrasMaiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 const letrasMaiusculas = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// exemplo de código que pega o valor de um texto digitado pelo usuário
-// var texto = prompt("Digite um texto: ");
-// var texto = entrada.value;
+const opcao = prompt("Digite um 1 para cifrar e 2 para decifrar");
+
+switch (opcao) {
+    case "1":
+        cifrar();
+        break;
+    case "2":
+        decifrar();
+        break;
+    default:
+        document.write("Ops, escolheu uma opção inexistente");
+        break;
+}
+
 
 function cifrar() {
-    const palavras = entrada.value.split(" ");
-    // se o usuário digita "TODAS EM TECH"
-    // retorno esperado ["TODAS", "EM", "TECH"]
-
-    // for (i = 0; i < palavras.length; i++) {
-    //     console.log(palavras[i], "texto genérico palavra");
-    // }
-
+    const palavras = entrada.split(" ");
     let cifrado = "";
-    let deslocamento = Number(chaveDeslocamento.value);
 
-    
-    //verifica se a chave de deslocamento existe e se alguma palavra foi digitada
-     // ver artigo https://blog.rocketseat.com.br/entendendo-falsy-e-truthy-no-javascript/
-    if (deslocamento && palavras.length > 0) {
-        for (itemPalavras of palavras) {
+    if (!isNaN(chaveDeslocamento) && palavras.length > 0) {
+        for (palavra of palavras) {
 
-            for (letra of itemPalavras) {
+            for (letra of palavra) {
                 // precisamos pegar o index da letra no array do alfabeto
                 const indexInicial = letrasMaiusculas.indexOf(letra);
                 if (indexInicial !== -1) {
-                    // vamos somar o index inicial com chave de deslocamento
+                    // vamos somar o index inicial com chave de chaveDeslocamento
                     // % retorna o resto da divisão
-                    let indexFinal = (indexInicial + deslocamento) % 26;
+                    let indexFinal = (indexInicial + chaveDeslocamento) % 26;
                     // cifrar = cifrar +  letrasMaiusculas[indexFinal]
                     cifrado += letrasMaiusculas[indexFinal];
                 } else {
-                    alert("Opa, digite uma letra maiúscula");
-                    limpar();
-                    // impede que o código fique em loop e trave o seu navegador
+                    alert("Poxa, não encontrei o que digitou");
                     return;
                 }
             }
             cifrado += " ";
         }
-        saida.value = cifrado;
+        document.write(cifrado);
         return;
     } else {
         alert("Digite uma chave e um texto para decifrar");
+        return;
     }
 }
 
-
 function decifrar() {
-    const palavrasDecifrar = entrada.value.split(" ");
+    const palavrasDecifrar = entrada.split(" ");
     let decifrado = "";
-    let deslocamento = Number(chaveDeslocamento.value);
 
-    if (deslocamento && palavrasDecifrar.length > 0) {
+    if (Number(chaveDeslocamento) && palavrasDecifrar.length > 0) {
+        // if (!isNaN(chaveDeslocamento) && palavrasDecifrar.length > 0) {
+
         for (palavra of palavrasDecifrar) {
             // laço que itera a palavra - letra a letra
             for (letra of palavra) {
@@ -81,28 +66,29 @@ function decifrar() {
                 let indexFinal;
 
                 // a condição garante que o index final é positivo
-                if (indexInicial !== -1 && indexInicial >= deslocamento) {
-                    indexFinal = (indexInicial - deslocamento);
+                if (indexInicial !== -1 && indexInicial >= chaveDeslocamento) {
+                    indexFinal = (indexInicial - chaveDeslocamento);
 
                     // decifrado = decifrado + letrasMaiusculas[indexFinal]
                     decifrado += letrasMaiusculas[indexFinal];
                     // a condição garante que a letra existe no array e a chave de deslocamento é maior que o index inicial;
-                    // a linha abaixo deixa mais limpo no lugar da linha 97 
+                    // a linha abaixo deixa mais limpo no lugar da linha 76
                     // } else if (indexInicial !== -1) {
-                } else if (indexInicial !== -1 && deslocamento > indexInicial) {
-                    indexFinal = letrasMaiusculas.length - (deslocamento - indexInicial);
+                } else if (indexInicial !== -1 && chaveDeslocamento > indexInicial) {
+                    indexFinal = letrasMaiusculas.length - (chaveDeslocamento - indexInicial);
                     decifrado += letrasMaiusculas[indexFinal];
                 } else {
                     // quando não encontra a letra no alfabeto, entra no else
                     alert("Poxa, não encontrei o que digitou");
-                    limpar();
                     return;
                 }
             }
             decifrado += " ";
         }
-        saida.value = decifrado;
+        document.write(decifrado);
+        return;
     } else {
         alert("Digite uma chave e um texto para decifrar");
+        return;
     }
 }
