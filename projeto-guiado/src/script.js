e194be0fe406985fa484a00cf90c73198b95c9d4
Lorena Rabelo <ola.lorenarabelo@gmail.com>
2021-04-08 21:52:18 -0300

    exemplo de switch case

--- projeto-guiado/src/script.js
+++ projeto-guiado/src/script.js
@@ -1,18 +1,26 @@
-const entrada = prompt("Digite um texto para cifrar ou decifrar: ");
+// const entrada = prompt("Digite um texto para cifrar ou decifrar: ");
 
-const chaveDeslocamento = Number(prompt("Digite um número para a chave de deslocamento: "));
+// const chaveDeslocamento = Number(prompt("Digite um número para a chave de deslocamento: "));
 
-// outra opção de gerar o array >> const letrasMaiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-const letrasMaiusculas = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// // outra opção de gerar o array >> const letrasMaiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+// const letrasMaiusculas = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-const opcao = prompt("Digite um 1 para cifrar e 2 para decifrar");
+
+
+const numero = Number(prompt("Digite um numero"));
+const numero2 = Number(prompt("Digite outro numero"));
+
+const opcao = prompt("Digite um 1 para somar um número com outro e digite 2 para multiplicar os números");
+
 
 switch (opcao) {
     case "1":
-        cifrar();
+        const soma = numero + numero2;
+        document.write(soma);
         break;
     case "2":
-        decifrar();
+        const multiplicar = numero * numero2;
+        console.log(multiplicar, "multiplicar");
         break;
     default:
         document.write("Ops, escolheu uma opção inexistente");
@@ -20,75 +28,77 @@ switch (opcao) {
 }
 
 
-function cifrar() {
-    const palavras = entrada.split(" ");
-    let cifrado = "";
 
-    if (!isNaN(chaveDeslocamento) && palavras.length > 0) {
-        for (palavra of palavras) {
 
-            for (letra of palavra) {
-                // precisamos pegar o index da letra no array do alfabeto
-                const indexInicial = letrasMaiusculas.indexOf(letra);
-                if (indexInicial !== -1) {
-                    // vamos somar o index inicial com chave de chaveDeslocamento
-                    // % retorna o resto da divisão
-                    let indexFinal = (indexInicial + chaveDeslocamento) % 26;
-                    // cifrar = cifrar +  letrasMaiusculas[indexFinal]
-                    cifrado += letrasMaiusculas[indexFinal];
-                } else {
-                    alert("Poxa, não encontrei o que digitou");
-                    return;
-                }
-            }
-            cifrado += " ";
-        }
-        document.write(cifrado);
-        return;
-    } else {
-        alert("Digite uma chave e um texto para decifrar");
-        return;
-    }
-}
+// function cifrar() {
+//     const palavras = entrada.split(" ");
+//     let cifrado = "";
 
-function decifrar() {
-    const palavrasDecifrar = entrada.split(" ");
-    let decifrado = "";
+//     if (!isNaN(chaveDeslocamento) && palavras.length > 0) {
+//         for (palavra of palavras) {
 
-    if (Number(chaveDeslocamento) && palavrasDecifrar.length > 0) {
-        // if (!isNaN(chaveDeslocamento) && palavrasDecifrar.length > 0) {
+//             for (letra of palavra) {
+//                 // precisamos pegar o index da letra no array do alfabeto
+//                 const indexInicial = letrasMaiusculas.indexOf(letra);
+//                 if (indexInicial !== -1) {
+//                     // vamos somar o index inicial com chave de chaveDeslocamento
+//                     // % retorna o resto da divisão
+//                     let indexFinal = (indexInicial + chaveDeslocamento) % 26;
+//                     // cifrar = cifrar +  letrasMaiusculas[indexFinal]
+//                     cifrado += letrasMaiusculas[indexFinal];
+//                 } else {
+//                     alert("Poxa, não encontrei o que digitou");
+//                     return;
+//                 }
+//             }
+//             cifrado += " ";
+//         }
+//         document.write(cifrado);
+//         return;
+//     } else {
+//         alert("Digite uma chave e um texto para decifrar");
+//         return;
+//     }
+// }
 
-        for (palavra of palavrasDecifrar) {
-            // laço que itera a palavra - letra a letra
-            for (letra of palavra) {
+// function decifrar() {
+//     const palavrasDecifrar = entrada.split(" ");
+//     let decifrado = "";
 
-                const indexInicial = letrasMaiusculas.indexOf(letra);
-                let indexFinal;
+//     if (Number(chaveDeslocamento) && palavrasDecifrar.length > 0) {
+//         // if (!isNaN(chaveDeslocamento) && palavrasDecifrar.length > 0) {
 
-                // a condição garante que o index final é positivo
-                if (indexInicial !== -1 && indexInicial >= chaveDeslocamento) {
-                    indexFinal = (indexInicial - chaveDeslocamento);
+//         for (palavra of palavrasDecifrar) {
+//             // laço que itera a palavra - letra a letra
+//             for (letra of palavra) {
 
-                    // decifrado = decifrado + letrasMaiusculas[indexFinal]
-                    decifrado += letrasMaiusculas[indexFinal];
-                    // a condição garante que a letra existe no array e a chave de deslocamento é maior que o index inicial;
-                    // a linha abaixo deixa mais limpo no lugar da linha 76
-                    // } else if (indexInicial !== -1) {
-                } else if (indexInicial !== -1 && chaveDeslocamento > indexInicial) {
-                    indexFinal = letrasMaiusculas.length - (chaveDeslocamento - indexInicial);
-                    decifrado += letrasMaiusculas[indexFinal];
-                } else {
-                    // quando não encontra a letra no alfabeto, entra no else
-                    alert("Poxa, não encontrei o que digitou");
-                    return;
-                }
-            }
-            decifrado += " ";
-        }
-        document.write(decifrado);
-        return;
-    } else {
-        alert("Digite uma chave e um texto para decifrar");
-        return;
-    }
-}
+//                 const indexInicial = letrasMaiusculas.indexOf(letra);
+//                 let indexFinal;
+
+//                 // a condição garante que o index final é positivo
+//                 if (indexInicial !== -1 && indexInicial >= chaveDeslocamento) {
+//                     indexFinal = (indexInicial - chaveDeslocamento);
+
+//                     // decifrado = decifrado + letrasMaiusculas[indexFinal]
+//                     decifrado += letrasMaiusculas[indexFinal];
+//                     // a condição garante que a letra existe no array e a chave de deslocamento é maior que o index inicial;
+//                     // a linha abaixo deixa mais limpo no lugar da linha 76
+//                     // } else if (indexInicial !== -1) {
+//                 } else if (indexInicial !== -1 && chaveDeslocamento > indexInicial) {
+//                     indexFinal = letrasMaiusculas.length - (chaveDeslocamento - indexInicial);
+//                     decifrado += letrasMaiusculas[indexFinal];
+//                 } else {
+//                     // quando não encontra a letra no alfabeto, entra no else
+//                     alert("Poxa, não encontrei o que digitou");
+//                     return;
+//                 }
+//             }
+//             decifrado += " ";
+//         }
+//         document.write(decifrado);
+//         return;
+//     } else {
+//         alert("Digite uma chave e um texto para decifrar");
+//         return;
+//     }
+// }
